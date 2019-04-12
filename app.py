c190c96f9772db116c8273d73d1819dc9f99c573
Ian Smith <ips89@outlook.com>
2019-04-12 16:53:19 -0500

    eighth commit

--- app.py
+++ app.py
@@ -12,7 +12,15 @@ def downloads():
 
 @app.route("/darkweblinks")
 def darkweblinks():
-	 return render_template('darkweblinks.html')
-
+	return render_template('darkweblinks.html')
+	 
+@app.route("/about")
+def about():
+	return render_template('about.html')
+	 
+@app.route("/destroypc")
+def destroypc():
+	return render_template('destroypc.html')	
+ 
 if __name__ == "__main__":
 		app.run(debug=True, host="0.0.0.0", port=80)
