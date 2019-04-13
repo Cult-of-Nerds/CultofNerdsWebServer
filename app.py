0c8e013516a2026ab36a265b6c9788cd366d3ee2
Ian Smith <ips89@outlook.com>
2019-04-13 14:18:03 -0500

    eleventh commit

--- app.py
+++ app.py
@@ -13,14 +13,22 @@ def downloads():
 @app.route("/darkweblinks")
 def darkweblinks():
 	return render_template('darkweblinks.html')
-	 
+
 @app.route("/about")
 def about():
 	return render_template('about.html')
 	 
 @app.route("/destroypc")
 def destroypc():
-	return render_template('destroypc.html')	
- 
+	return render_template('destroypc.html')
+
+@app.route("/explosiverecipes")
+def explosiverecipes():
+	return render_template('explosiverecipes.html')
+	
+@app.route("/hateschool")
+def hateschool():
+	return render_template('hateschool.html')
+
 if __name__ == "__main__":
 		app.run(debug=True, host="0.0.0.0", port=80)
